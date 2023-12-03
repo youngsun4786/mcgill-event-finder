ef0e118f6ba9b3d9268d88afbac1aa51cad06c9d
dkfndwl <ahreuml0304@gmail.com>
2023-12-03 05:15:24 -0500

    post item styling

---
+++ frontend/src/app/views/post/components/post-item/components/filter/filter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-filter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.css'
+})
+export class FilterComponent {
+
+}
