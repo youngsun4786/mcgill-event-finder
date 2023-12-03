ca8959c6ae4710e92c30fafe6d95e3908a474db0
huetimes3 <ttricamavrodin@gmail.com>
2023-12-02 21:42:34 -0500

    modal progress

---
+++ frontend/src/app/views/main/create-event/create-event.component.ts
@@ -0,0 +1,81 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { Post } from '../../../models/post.models';
+
+const createEventToggleAnimation = [
+	trigger('overlayToggle', [
+		state('open', style({
+			opacity: 0.25
+		})),
+		state('closed', style({
+			opacity: 0
+		})),
+		transition('open => closed', [
+			animate('300ms ease-in')
+		]),
+		transition('closed => open', [
+			animate('200ms ease-in')
+		])
+	]),
+	trigger('fadeInOut', [
+    state('open', style({
+			transform: 'translateY(0)',
+      opacity: 1
+		})),
+		state('closed', style({
+			transform: 'translateY(30px)',
+      opacity: 0
+		})),
+		transition('open => closed', [
+			animate('300ms ease-in')
+		]),
+		transition('closed => open', [
+			animate('200ms ease-in')
+		])
+  ])
+]
+
+@Component({
+  selector: 'app-create-event',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, NgSelectModule],
+  animations: createEventToggleAnimation,
+  templateUrl: './create-event.component.html',
+  styleUrl: './create-event.component.css'
+})
+export class CreateEventComponent {
+  @Input('showCreateEvent') showCreateEvent: boolean = false;
+  @Output() showCreateEventChange = new EventEmitter<boolean>();
+
+  newEventForm: FormGroup;
+  eventDayType: string = 'singleday';
+
+  constructor(fb : FormBuilder) {
+    this.newEventForm = fb.group({
+      title: ['', Validators.required],
+      startDate: [''],
+      startTime: [''],
+      endDate: [''],
+      endTime: [''],
+      location: [''],
+      description: [''],
+      tags: [''],
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+  closeCreateEvent() {
+    this.showCreateEvent = false;
+    this.showCreateEventChange.emit(this.showCreateEvent);
+  }
+
+  onEventCreate() {
+
+  }
+}
--- frontend/src/app/views/main/main.component.ts
+++ frontend/src/app/views/main/main.component.ts
@@ -4,13 +4,27 @@ import { StorageService } from '../../services/storage.service';
 import { AuthService } from '../../services/auth.service';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { CreateEventComponent } from './create-event/create-event.component';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CreateEventComponent, MatButtonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
 export class MainComponent {
 
+  createEventOpen: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  createEvent() {
+    this.createEventOpen = !this.createEventOpen;
+  }
 }
