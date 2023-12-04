5c9f57c72aa7b185afb43f41af69c707fb7b1c6e
dkfndwl <ahreuml0304@gmail.com>
2023-12-04 06:11:57 -0500

    added upcoming components (not filtered), further styling on nav bar and images

--- frontend/src/app/views/post/components/display-post/display-post.component.ts
+++ frontend/src/app/views/post/components/display-post/display-post.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Post } from '../../../../models/post.models';
 import { PostItemComponent } from '../post-item/post-item.component';
+import { UpcomingPostComponent } from '../upcoming-post/upcoming-post.component';
 
 @Component({
   selector: 'app-display-post',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, PostItemComponent],
+  imports: [CommonModule, HttpClientModule, PostItemComponent, UpcomingPostComponent],
   templateUrl: './display-post.component.html',
   styleUrl: './display-post.component.css',
 })
