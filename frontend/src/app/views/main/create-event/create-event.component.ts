42c943ef877b693c88b4da796dceb3c14b8e4585
huetimes3 <ttricamavrodin@gmail.com>
2023-12-04 02:04:16 -0500

    finished create modal! only backend call leftttt

--- frontend/src/app/views/main/create-event/create-event.component.ts
+++ frontend/src/app/views/main/create-event/create-event.component.ts
@@ -1,12 +1,15 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { trigger, state, style, animate, transition } from '@angular/animations';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Post } from '../../../models/post.models';
+import { EventStatusType, Post } from '../../../models/post.models';
 import { DateRange, MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { NgClickOutsideDirective } from 'ng-click-outside2';
+import { User } from '@app/models/user.models';
+import { HttpClient } from '@angular/common/http';
+import { httpOptions } from '@app/services/auth.service';
 
 
 const createEventToggleAnimation = [
@@ -60,6 +63,7 @@ const createEventToggleAnimation = [
 export class CreateEventComponent {
   @Input('showCreateEvent') showCreateEvent: boolean = false;
   @Output() showCreateEventChange = new EventEmitter<boolean>();
+  httpClient = inject(HttpClient);
 
   newEventForm: FormGroup;
   eventDayType: string = 'singleday';
@@ -74,14 +78,17 @@ export class CreateEventComponent {
   minDate: Date = new Date();
   maxDate: Date = new Date();
 
+  isIncomplete: boolean = false;
+  timeError: boolean = false;
+
   constructor(fb : FormBuilder) {
     this.newEventForm = fb.group({
       title: ['', Validators.required],
-      startDate: [''],
-      startTime: [''],
-      endDate: [''],
-      endTime: [''],
-      location: [''],
+      startDate: ['', Validators.required],
+      startTime: ['', Validators.required],
+      endDate: ['', Validators.required],
+      endTime: ['', Validators.required],
+      location: ['', Validators.required],
       description: [''],
       tags: [''],
     });
@@ -144,14 +151,56 @@ export class CreateEventComponent {
     return new Date();
   }
 
+  getTime(t: string) {
+    if (t === 'min') {
+      console.log(this.newEventForm.controls['startTime'].value)
+      return this.newEventForm.controls['startTime'].value || '00:00';
+    }
+
+    if (t === 'max') {
+      return this.newEventForm.controls['endTime'].value || '23:59';
+    }
+  }
+
   // TODO: prevent date from being before startdate
 
   closeCreateEvent() {
     this.showCreateEvent = false;
+    this.isIncomplete = false;
     this.showCreateEventChange.emit(this.showCreateEvent);
   }
 
   onEventCreate() {
+    this.isIncomplete = !this.newEventForm.valid;
+    this.timeError = this.eventDayType == 'singleday' && (this.newEventForm.controls['startTime'].value > this.newEventForm.controls['endTime'].value);
 
+    if (this.isIncomplete || this.timeError) return;
+
+
+    let newPost: Post = {
+      title: this.newEventForm.controls['title'].value,
+      location: this.newEventForm.controls['location'].value,
+      startDate: new Date(this.newEventForm.controls['startDate'].value + ' ' + this.newEventForm.controls['startTime'].value),
+      endDate: new Date(this.newEventForm.controls['endDate'].value + ' ' + this.newEventForm.controls['endTime'].value),
+      createdAt: new Date(),
+      tags: this.newEventForm.controls['tags'].value,
+      status: EventStatusType.SCHEDULED,
+      author: new Object() as User,
+    }
+    this.createEvent(newPost);
+  }
+
+  createEvent(post: Post) {
+    this.httpClient
+      .post(`http://localhost:8000/posts`, post, httpOptions)
+      .subscribe({
+        next: () => {
+          this.closeCreateEvent();
+        },
+        error: (error: any) => {
+          alert(error.toString().replace(/['"]+/g, ''));
+          console.error(error);
+        },
+      });
   }
 }
