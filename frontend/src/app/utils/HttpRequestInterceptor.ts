d1b49ed5339653096ca53bde436d785117da69b0
Youngsun Yoo <youngsun123q@gmail.com>
2023-12-11 05:49:39 -0500

    remove http option

--- frontend/src/app/utils/HttpRequestInterceptor.ts
+++ frontend/src/app/utils/HttpRequestInterceptor.ts
@@ -15,7 +15,6 @@ export class HttpRequestInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     req = req.clone({
-      url: req.url.replace('http://', 'https://'),
       withCredentials: true,
     });
 
