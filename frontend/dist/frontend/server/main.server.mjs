866468624b893ca0d3aaeae39b7e079f699a650f
Youngsun Yoo <youngsun123q@gmail.com>
2023-12-11 02:55:38 -0500

    add production version

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TEOIR4FF.mjs";import"./chunk-2FO7G7NZ.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-OJQVBF6J.mjs";import"./chunk-2FO7G7NZ.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { enableProdMode } from '@angular/core';
+import { environment } from '@env/environment';
 
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
