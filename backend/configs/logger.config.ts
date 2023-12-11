3319bdbe51b68aede3447568ee1670adeaf06759
Youngsun Yoo <youngsun123q@gmail.com>
2023-12-11 00:42:23 -0500

    transport pino test

--- backend/configs/logger.config.ts
+++ backend/configs/logger.config.ts
@@ -1,21 +1,13 @@
 import logger from "pino";
 import dotenv from "dotenv";
-import pretty from "pino-pretty";
 
 dotenv.config();
-
-const stream = pretty({
-  levelFirst: true,
-  colorize: true,
-  ignore: "time,hostname,pid",
-});
 
 const level = process.env.NODE_ENV === "production" ? "info" : "debug";
 
 export const log = logger({
   name: "McEvent",
   level,
-  stream,
   base: { pid: false },
   timestamp: () => `,"time":"${Date.now().toFixed(0)}"`,
 });
